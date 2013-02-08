4bc8f8f2492ea4f109b54f959eb31adacd4fd6b8
Chris Brulak <chris.brulak@ama.ab.ca>
2013-02-08 16:21:18 -0700

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VanityExperiment::Application.config.secret_token = '9541d186a530665f95728e04f815ece66fd14202f07b430951797422e8762eb283ae00acb8d8c51cb23e5ad8e6dc6ed69263fd25e76dd3bc269f602ef63aee24'
